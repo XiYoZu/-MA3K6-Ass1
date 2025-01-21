7e6ee5c3500406b778ebaf810fb566a136f9c769
XiYoZu <139477511+XiYoZu@users.noreply.github.com>
2025-01-21 09:00:34 +0000

    init

--- problem.py
+++ problem.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import numpy as np
+
+
+start = [1,1]
+repeat = False
+while (repeat == False):
+    start.append(sum(start[-2:]) % 10)
+    if (start[-2] == 1) and (start[-1] == 1):
+        repeat = True
+print(start)
